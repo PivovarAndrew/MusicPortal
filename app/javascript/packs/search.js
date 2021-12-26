eb6caccf8c6f57cc5b108409eab1bdb37c3cd29d
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2021-12-26 04:15:59 +0300

    Style modify for search results panel.

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,6 +1,6 @@
-import {openSidebar, closeSidebar} from './sidebar.js'
+import { openSidebar, closeSidebar } from './sidebar.js'
 
-window.searchAlbum = function(text) {
+window.searchAlbum = function (text) {
     $.ajax({
         url: '/_searched_albums',
         type: 'GET',
@@ -19,20 +19,36 @@ window.searchAlbum = function(text) {
         }
 
     });
-return false;
+    return false;
 }
 
+const searced_results_sidebar_id = "searched-results-sidebar"
+
 document.addEventListener("DOMContentLoaded", function (event) {
-    const searced_results_sidebar_id = "searched-results-sidebar"
-    $('#search').on('input', function () {
+    const search = $('#search')
+
+    search.on('input', function () {
         openSidebar(searced_results_sidebar_id);
         if ($('#search').val() == "") {
             closeSidebar(searced_results_sidebar_id);
         }
-
     });
-    
+
+    search.click(function () {
+        if (search.val() != "") {
+            openSidebar(searced_results_sidebar_id);
+        }
+    });
+
     $('#searched-albums-sidebar-close-button').click(function () {
         closeSidebar(searced_results_sidebar_id);
     });
+});
+
+document.addEventListener('click', function (event) {
+    const search = document.getElementById('search')
+    const searched_results_panel = document.getElementById("searched-results-sidebar")
+    if (!(search.contains(event.target) || searched_results_panel.contains(event.target))) {
+        closeSidebar(searced_results_sidebar_id);
+    }
 });
