8da19ed832d7a75d6da82f7d91609e315b115254
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2021-12-29 05:09:11 +0300

    Rename unsed attributes constant.

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,14 +1,15 @@
 # Class required for search injection
 class SearchService
-  WASTE_ATTRIBUTES = %w[id created_at updated_at description image_preview_url].freeze
+  OUT_OF_USE_ATTRIBUTES = %w[id created_at updated_at description image_preview_url].freeze
 
   def search_album(text)
     albums = Album.all
     results = []
+    Album.where
     albums[0].attribute_names.each do |attribute|
-      next if WASTE_ATTRIBUTES.include? attribute
+      next if OUT_OF_USE_ATTRIBUTES.include? attribute
 
-      results.push albums.where "#{attribute}::text ILIKE '%#{text}%'"
+      results.push albums.where "#{attribute}::text ILIKE '%#{text}%'", text
     end
     results.flatten!
   end
