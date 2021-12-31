36798a1f8e4290600ee9ed33613cef9596a4c1c2
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2021-12-31 17:43:10 +0300

    Combine user profile and user views.

--- app/helpers/user_profiles_helper.rb
+++ app/helpers/user_profiles_helper.rb
@@ -4,4 +4,16 @@ module UserProfilesHelper
   def default_avatar(user_profile)
     user_profile.avatar.url.nil? ? DEFAULT_AVATAR_IMAGE : user_profile.avatar.url
   end
+
+  def resource
+    @resource ||= current_user
+  end
+
+  def resource_name
+    current_user.user_profile.name || :user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
