6675998854f635d2ac526077a3ff3e0b9a3e33d8
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-05 02:57:45 +0300

    Add confirmation properties to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_020940) do
+ActiveRecord::Schema.define(version: 2022_01_04_234425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,19 @@ ActiveRecord::Schema.define(version: 2021_12_27_020940) do
     t.index ["user_id"], name: "index_user_albums_on_user_id"
   end
 
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "avatar"
+    t.string "name"
+    t.string "surname"
+    t.string "nickname"
+    t.date "date_of_birthday"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -96,8 +109,16 @@ ActiveRecord::Schema.define(version: 2021_12_27_020940) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_profiles", "users"
 end
