6e76d1a8febb6907b2fc871aaaef54e1c7b44747
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-05 04:49:58 +0300

    Smtp setup for sending

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
   end
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 end
