d91c987cd9e86afd5f54954048a1a7c9c9740ced
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-06 04:08:12 +0300

    Add comments controller.

--- db/migrate/20220106005504_create_comments.rb
+++ db/migrate/20220106005504_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.text :content
+      t.text :content, null: false
       t.references :album, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
