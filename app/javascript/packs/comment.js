ad94f722a9ee5c575a358781f1d19e4b1c6a4351
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-06 23:25:25 +0300

    Add reply button to comment.

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -19,3 +19,23 @@ window.addComment = function(album_id) {
     });
 return false;
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    let commentForms = $('.comment-form');
+    let replyButton = $('.reply-button');
+    commentForms.hide();
+
+    replyButton.click(function () {
+        let reply_text = "Reply"
+        let hide_text = "Hide"
+        let idFor = $($(this).attr('data-for'));
+        let currentButton = $(this);
+        idFor.slideToggle(400, function () {
+            idFor.is(':visible') ? currentButton.html(`<label name="${hide_text.toLowerCase()}", 
+            class="${reply_text.toLowerCase()}">${hide_text}</label>`) : currentButton.html(
+            `<label name="${reply_text.toLowerCase()}", class="${reply_text.toLowerCase()}">${reply_text}</label>`);
+        })
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+});
