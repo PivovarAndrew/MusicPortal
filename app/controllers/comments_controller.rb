58282536a40cbac0a763087fb02963e2b125710d
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-06 06:12:01 +0300

    Create base comment system.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.new(comment_params)
+    format.html { render :new, status: :unprocessable_entity } unless @comment.save
+    redirect_to @comment.album
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(album_id: params[:album_id])
+  end
 end
