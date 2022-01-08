224ff4de1bb976a067ab9e6107bd209af39fe7c3
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-09 02:10:02 +0300

    Add mail preview.

--- app/mailers/interest_subscription_mailer.rb
+++ app/mailers/interest_subscription_mailer.rb
@@ -1,5 +1,5 @@
 class InterestSubscriptionMailer < ApplicationMailer
-  def daily_album
+  def daily_album_mailer
     @user = params[:user]
     @album = params[:album]
     mail(to: @user.email, subject: "Daily album!")
--- test/mailers/previews/interest_subscription_mailer_preview.rb
+++ test/mailers/previews/interest_subscription_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/interest_subscription_mailer
 class InterestSubscriptionMailerPreview < ActionMailer::Preview
-
+  def daily_album_mailer
+    InterestSubscriptionMailer.with(user: User.all.sample, album: Album.all[0]).daily_album_mailer
+  end
 end
