555871770918601792e2a19aca39916ee755c605
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-08 04:47:02 +0300

    Add option to user create from provider data info.

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,15 @@ class User < ApplicationRecord
   end
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[github google_oauth2]
+
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0..20]
+    end
+  end
 end
