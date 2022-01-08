28e462d581eaab1baf8f5ee53c14dd9d34a296bf
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-08 18:47:22 +0300

    Add get rating helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  MIN_RATING_VALUE = 1
+
+  def get_rating(album)
+    likes_count = likes_count.zero? ? MIN_RATING_VALUE : album.likes.count
+    dislikes_count = dislikes_count.zero? ? MIN_RATING_VALUE : album.dislikes.count
+    ((likes.count / dislikes.count) * 10).round(0)
+  end
 end
