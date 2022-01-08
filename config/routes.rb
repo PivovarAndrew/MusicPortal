accfcc9d304ec27f5e4bf1c72d0be2040d7d487d
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-09 02:31:49 +0300

    Add route to sidekiq web-interface.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   resources :user_albums
   resources :albums
@@ -8,5 +10,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :tracks
   resources :users
+  mount Sidekiq::Web => "/sidekiq"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
