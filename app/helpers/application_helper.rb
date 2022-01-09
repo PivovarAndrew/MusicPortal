d0caadfb77bcc318724ef51d45d7d9e0305d4d84
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-09 17:07:43 +0300

    Restore styling for album show.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,7 @@ module ApplicationHelper
     dislikes_count = album.dislikes.count
     total_ratings = likes_count + dislikes_count
     total_ratings.zero? ? NO_RATING_VALUE : (MAX_RATING_VALUE * likes_count / total_ratings).round
+  end
 
   def default_avatar(user_profile)
     user_profile.avatar.url || DEFAULT_AVATAR_IMAGE
