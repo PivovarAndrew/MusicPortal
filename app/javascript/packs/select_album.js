85a7ae28a297f8b2f661bdf9a693c70ea77dc194
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-10 07:15:43 +0300

    Update open/close sidebar events as delegates.

--- app/javascript/packs/select_album.js
+++ app/javascript/packs/select_album.js
@@ -2,11 +2,11 @@ import {openSidebar, closeSidebar} from './sidebar.js'
 
 document.addEventListener("DOMContentLoaded", function (event) {
     const SELECTED_ALBUM_SIDEBAR_ID = "selected-album-sidebar"
-    $('.selected-album-sidebar-open').click(function () {
+    $(document).on('click', '.selected-album-sidebar-open',function () {
         openSidebar(SELECTED_ALBUM_SIDEBAR_ID);
     });
     
-    $('#selected-album-sidebar-close-button').click(function () {
+    $(document).on('click', '#selected-album-sidebar-close-button', function () {
         closeSidebar(SELECTED_ALBUM_SIDEBAR_ID);
     });
 });
