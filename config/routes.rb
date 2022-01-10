4f9efe4e6a6af1f08b4e553e7657c43ec729e2ff
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-10 06:23:53 +0300

    Create nested routes album-track relation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   resources :user_albums
-  resources :albums
+  resources :albums do
+    resources :tracks
+  end
   get "/_album_tracks", to: "albums#_album_tracks"
   get "/_pagy_filter_albums_grid", controller: "pages", action: "_pagy_filter_albums_grid"
   get "/_searched_albums", to: "pages#_searched_albums"
   post "/_add_album_to_playlist", to: "albums#_add_album_to_playlist"
   devise_for :users
   root to: "pages#home"
-  resources :tracks
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
