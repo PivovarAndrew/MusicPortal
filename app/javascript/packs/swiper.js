94bc9bf5360a5d28edc99cdcb445f3634dfacd4c
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-12 05:32:11 +0300

    Create carousel with albums.

---
+++ app/javascript/packs/swiper.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var swiperBottomScrollbarFull = new Swiper('.swiper-bottom-scrollbar-full', {
+        allowTouchMove: true,
+        slidesPerView: 'auto',
+        grabCursor: true,
+        preventClicks: true,
+        spaceBetween: 30,
+        keyboardControl: true,
+        speed: 1000,
+        pagination: {
+            el: null
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+            hide: false,
+            snapOnRelease: true
+        },
+        mousewheel: {
+            enable: true
+        },
+        keyboard: {
+            enabled: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        breakpoints: {
+            767: {
+                scrollbar: {
+                    hide: true
+                },
+                spaceBetween: 0,
+                autoHeight: true,
+                centeredSlides: false,
+                slidesOffsetAfter: 85
+            }
+        },
+        on: {
+            resize: function () {
+                fsdsasa
+                var windowWidth = $(window).width();
+                if (windowWidth <= 767) {
+                    swiperBottomScrollbarFull.detachEvents();
+                } else {
+                    swiperBottomScrollbarFull.attachEvents();
+                }
+                swiperBottomScrollbarFull.update();
+            }
+        }
+    });
+});
