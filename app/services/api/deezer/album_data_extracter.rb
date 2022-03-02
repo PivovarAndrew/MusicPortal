c9e88e0acf3e18675061a17d09be9efd989de7eb
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-03-02 20:30:59 +0300

    Transfer deezer api services in another directory.

--- app/services/api/deezer/album_data_extracter.rb
+++ app/services/api/deezer/album_data_extracter.rb
@@ -5,8 +5,16 @@ module AlbumDataService
     end
 
     def albums
+      albums_without_tracks.each_with_index do |searched_album, counter|
+        searched_album.tracks = album_tracks[counter]
+      end
+    end
+
+    def albums_without_tracks
       @album_data.map { |album_info| album_info[0][:album] }
     end
+
+    private
 
     def album_tracks
       @album_data.map { |album_info| album_info[0][:tracks] }
