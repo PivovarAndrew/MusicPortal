d1fffb28e3cb52074f46a585d80edfcd211525c7
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-03-15 14:53:11 +0300

    Create line charts.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -39,5 +39,18 @@ class PagesController < ApplicationController
     @album_genres_groupped_by_comments_count = Album.joins(:comments).group(:main_genre).count
     @album_countries_groupped_by_comments_count = Album.joins(:comments).group(:countries).count
     @album_age_restrictions_groupped_by_comments_count = Album.joins(:comments).group(:age_restrictions).count
+
+    @user_create_groupped_by_monthes = User.group_by_month(:created_at, format: "%b %Y").count
+    @like_create_groupped_by_monthes = Like.group_by_month(:created_at, format: "%b %Y").count
+    @dislike_create_groupped_by_monthes = Dislike.group_by_month(:created_at, format: "%b %Y").count
+    @comment_create_groupped_by_monthes = Comment.group_by_month(:created_at, format: "%b %Y").count
+    @album_create_groupped_by_monthes = Album.group_by_month(:created_at, format: "%b %Y").count
+    @track_create_groupped_by_monthes = Track.group_by_month(:created_at, format: "%b %Y").count
+    @user_create_groupped_by_weeks = User.group_by_week(:created_at).count
+    @like_create_groupped_by_weeks = Like.group_by_week(:created_at).count
+    @dislike_create_groupped_by_weeks = Dislike.group_by_week(:created_at).count
+    @comment_create_groupped_by_weeks = Comment.group_by_week(:created_at).count
+    @album_create_groupped_by_weeks = Album.group_by_week(:created_at).count
+    @track_create_groupped_by_weeks = Track.group_by_week(:created_at).count
   end
 end
